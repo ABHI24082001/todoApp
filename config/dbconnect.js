f659f754422b62032bbd3984fbb356311fa0fb08
Abhishek <sonukr24082001@gmail.com>
2024-06-11 16:14:45 +0530

    delte

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const dbConnect =  ()=>{
 try {
     const connection =  mongoose.connect(
-      "mongodb+srv://sonukr24082001:Abhi%40123@cluster0.hamldut.mongodb.net/texi"
+      "mongodb+srv://sonukr24082001:Abhi%40123@cluster0.hamldut.mongodb.net/todo"
     );
    if(connection){
     console.log("DB connection established")
--- server.js
+++ server.js
@@ -4,11 +4,6 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import userRouter  from './routes/user.routes.js'
-import productRouter from './routes/product.routes.js'
-import CategoryRouter from './routes/category.routes.js'
-import Order from  './routes/orderss.routes.js'
-import Cart from './routes/cart.routes.js'
-import Review from './routes/review.routes.js'
 
 
 
@@ -22,11 +17,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/users", productRouter)
-app.use("/api/v1/users", CategoryRouter);
-app.use("/api/v1/users", Order);
-app.use("/api/v1/users", Cart);
-app.use("/api/v1/users", Review);
+
 
 
 const PORT = 8000;
